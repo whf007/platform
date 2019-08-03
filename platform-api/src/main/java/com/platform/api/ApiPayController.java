a01f1887ff8ef72879ad546f85650e3ee20204ba
whf007 <274418259@qq.com>
2019-08-03 23:37:35 +0800

    order

--- platform-api/src/main/java/com/platform/api/ApiPayController.java
+++ platform-api/src/main/java/com/platform/api/ApiPayController.java
@@ -63,7 +63,7 @@ public class ApiPayController extends ApiBaseAction {
             return toResponsObject(400, "订单已取消", "");
         }
 
-        if (orderInfo.getPay_status() != 0) {
+        if (orderInfo.getPay_status() == 2) {
             return toResponsObject(400, "订单已支付，请不要重复操作", "");
         }
 
